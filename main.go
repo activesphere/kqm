0a906841bcee1f39628cb7efda2bf3160c9f6529
Omkar <omkar@activesphere.com>
2017-09-14 12:39:37 +0530

    Add basic demo calls to test QSM

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	
+	qsm, err := NewQueueSizeMonitor([]string{"localhost:9092"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = qsm.GetOffsetMetadata()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
