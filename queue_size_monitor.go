75c8cbe16abd8b481c2c0d302f5fd89b25681d0e
Omkar <omkar@activesphere.com>
2017-09-14 12:39:22 +0530

    Add methods to parse and fetch Consumer offsets

--- queue_size_monitor.go
+++ queue_size_monitor.go
@@ -2,53 +2,170 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"bytes"
+	"encoding/binary"
+	"log"
 	"github.com/Shopify/sarama"
 )
+
+// ConsumerOffsetTopic : provides the topic name of the Offset Topic.
+const ConsumerOffsetTopic = "__consumer_offsets"
 
 // QueueSizeMonitor : Defines the type for Kafka Queue Size 
 // Monitor implementation using Sarama.
 type QueueSizeMonitor struct {
-	Client sarama.Client
-	Groups []ConsumerGroup
+	Client              sarama.Client
+	wgConsumerMessages  sync.WaitGroup
 }
 
 // NewQueueSizeMonitor : Returns a QueueSizeMonitor with an initialized client
 // based on the comma-separated brokers (eg. "localhost:9092")
-func (qsm *QueueSizeMonitor) NewQueueSizeMonitor(brokers []string) (*QueueSizeMonitor, error) {
+func NewQueueSizeMonitor(brokers []string) (*QueueSizeMonitor, error) {
 	config := sarama.NewConfig()
 	client, err := sarama.NewClient(brokers, config)
-	qsm = &QueueSizeMonitor{}
+	qsm := &QueueSizeMonitor{}
 	qsm.Client = client
 	return qsm, err
 }
 
-// ConnectBroker : Connects to a broker
-func (qsm *QueueSizeMonitor) ConnectBroker(broker *sarama.Broker) error {
-	if ok, _ := broker.Connected(); ok {
-		return nil
-	}
-
-	err := broker.Open(qsm.Client.Config())
+// GetConsumerPartitionOffsets : Appropriate description to be added.
+func (qsm *QueueSizeMonitor) GetConsumerPartitionOffsets() {
+	log.Println("Started getting consumer partition offsets...")
+	
+	partitions, err := qsm.Client.Partitions(ConsumerOffsetTopic)
 	if err != nil {
-		return err
+		log.Fatalln("Error occured while getting client partitions.", err)
+		return
 	}
 
-	connected, err := broker.Connected()
+	consumer, err := sarama.NewConsumerFromClient(qsm.Client)
 	if err != nil {
-		return err
+		log.Fatalln("Error occured while creating new client consumer.", err)
+		return
 	}
 
-	if !connected {
-		return fmt.Errorf("Failed to connect to broker: %#v", broker.Addr())
+	partitionsConsumers := make([]sarama.PartitionConsumer, len(partitions))
+	pOffsetChannel := make(chan *PartitionOffset)
+
+	getConsumerMessages := func(consumer sarama.PartitionConsumer) {
+		defer qsm.wgConsumerMessages.Done()
+		for message := range consumer.Messages() {
+			log.Println("Message received.", message)
+			go qsm.formatConsumerOffsetMessage(message, pOffsetChannel)
+		}
+	}
+
+	getConsumerErrors := func(consumer sarama.PartitionConsumer) {
+		defer qsm.wgConsumerMessages.Done()
+		for err := range consumer.Errors() {
+			log.Fatalln("Error occured in Partition Consumer:", err)
+		}
+	}
+
+	for index, partition := range partitions {
+		pConsumer, err := consumer.ConsumePartition(ConsumerOffsetTopic, partition, sarama.OffsetNewest)
+		if err != nil {
+			log.Fatalln("Error occured while consuming partition.", err)
+		}
+		partitionsConsumers[index] = pConsumer
+		qsm.wgConsumerMessages.Add(2)
+		go getConsumerMessages(pConsumer)
+		go getConsumerErrors(pConsumer)
+	}
+
+	qsm.wgConsumerMessages.Wait()
+	for _, pConsumer := range partitionsConsumers {
+		pConsumer.AsyncClose()
 	}
 }
 
-// GetConsumerGroups : Returns a list of Consumer Groups associated with the
-// client.
-func (qsm *QueueSizeMonitor) GetConsumerGroups() ([]ConsumerGroup, error) {
-	brokers := qsm.Client.Brokers()
-	consumerGroups := make([]ConsumerGroup, 10)
-	for _, broker := range brokers {
-		
+// Burrow-based Consumer Offset Message parser function.
+func (qsm *QueueSizeMonitor) formatConsumerOffsetMessage(
+	message *sarama.ConsumerMessage, pOffsetChannel chan *PartitionOffset) {
+
+	log.Println("Formatting Consumer Offset Message...")
+
+	readString := func(buf *bytes.Buffer) (string, error) {
+		var strlen uint16
+		err := binary.Read(buf, binary.BigEndian, &strlen)
+		if err != nil {
+			return "", err
+		}
+		strbytes := make([]byte, strlen)
+		n, err := buf.Read(strbytes)
+		if (err != nil) || (n != int(strlen)) {
+			return "", fmt.Errorf("string underflow")
+		}
+		return string(strbytes), nil
 	}
+
+	logError := func(err error) {
+		log.Fatalln("Error while parsing message.", err)
+	}
+
+	var keyver, valver uint16
+	var group, topic string
+	var partition uint32
+	var offset, timestamp uint64
+
+	buf := bytes.NewBuffer(message.Key)
+	err := binary.Read(buf, binary.BigEndian, &keyver)
+	switch keyver {
+	case 0, 1:
+		group, err = readString(buf)
+		if err != nil {
+			logError(err)
+			return
+		}
+		topic, err = readString(buf)
+		if err != nil {
+			logError(err)
+			return
+		}
+		err = binary.Read(buf, binary.BigEndian, &partition)
+		if err != nil {
+			logError(err)
+			return
+		}
+	case 2:
+		logError(err)
+		return
+	default:
+		logError(err)
+		return
+	}
+
+	buf = bytes.NewBuffer(message.Value)
+	err = binary.Read(buf, binary.BigEndian, &valver)
+	if (err != nil) || ((valver != 0) && (valver != 1)) {
+		logError(err)
+		return
+	}
+	err = binary.Read(buf, binary.BigEndian, &offset)
+	if err != nil {
+		logError(err)
+		return
+	}
+	_, err = readString(buf)
+	if err != nil {
+		logError(err)
+		return
+	}
+	err = binary.Read(buf, binary.BigEndian, &timestamp)
+	if err != nil {
+		logError(err)
+		return
+	}
+
+	partitionOffset := &PartitionOffset{
+		Topic:     topic,
+		Partition: int32(partition),
+		Group:     group,
+		Timestamp: int64(timestamp),
+		Offset:    int64(offset),
+	}
+
+	log.Println("Consumer Offset from formatted message:", partitionOffset.Offset)
+	pOffsetChannel <- partitionOffset
 }
