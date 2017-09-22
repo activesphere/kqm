a8614f37a01a6a7d3b1bed49acbfc96f23f3be87
Omkar <omkar@activesphere.com>
2017-09-22 13:34:04 +0530

    Update commands list

--- kafka-commands.sh
+++ kafka-commands.sh
@@ -4,7 +4,10 @@ Before using any of these commands, make sure you are in the kafka dir.
 nohup bin/kafka-server-start.sh config/server.properties > running.log 2>&1 &
 
 # Create a Kafka Topic:
-bin/kafka-topics.sh --create --topic topicname --zookeeper localhost:2181 --partitions 4 --replication-factor 1
+bin/kafka-topics.sh --create --topic topic_1 --zookeeper localhost:2181 --partitions 4 --replication-factor 4
+
+# Delete a Kafka Topic:
+bin/kafka-topics.sh --delete --topic topic_1 --zookeeper localhost:2181
 
 # Create a Kafka Producer:
 bin/kafka-console-producer.sh --topic topic_1 --broker-list localhost:9092
@@ -12,14 +15,14 @@ bin/kafka-console-producer.sh --topic topic_1 --broker-list localhost:9092
 # Create a Kafka Non-Zookeeper (New) Consumer:
 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic topic_1 --from-beginning
 
-# List Non-Zookeeper (New) Consumer Groups (including the ones created 
+# List Non-Zookeeper (New) Consumer Groups (including the ones created
 # when a new consumer is created):
 bin/kafka-consumer-groups.sh --list --bootstrap-server localhost:9092
 
 # Create a Kafka Zookeeper (Old) Consumer:
 bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic topic_1 --from-beginning
 
-# List Non-Zookeeper (Old) Consumer Groups (including the ones created 
+# List Non-Zookeeper (Old) Consumer Groups (including the ones created
 # when a new consumer is created):
 bin/kafka-consumer-groups.sh --list --zookeeper localhost:2181
 
