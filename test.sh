a539fd3c7c6244ef1cab947398a54da106103577
Omkar <omkar@activesphere.com>
2017-12-07 20:41:37 +0530

    Suppress dependency logging

--- test.sh
+++ test.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-# Update dependencies
-apt-get clean && apt-get update
-apt-get install -y python zookeeper zookeeperd wget software-properties-common git net-tools
-
-# Install Go
-add-apt-repository -y ppa:longsleep/golang-backports
-apt-get update
-apt-get install -y golang-go
-
-# Install Kafka
-wget http://redrockdigimark.com/apachemirror/kafka/0.11.0.2/kafka_2.11-0.11.0.2.tgz
-tar xvf kafka_2.11-0.11.0.2.tgz
-mv kafka_2.11-0.11.0.2 kafka
-
-# Install librdkafka v0.11.1
-wget https://github.com/edenhill/librdkafka/archive/v0.11.1.tar.gz
-tar xvf v0.11.1.tar.gz
-pushd /kqm/librdkafka-0.11.1
-./configure --prefix /usr && make && make install
-popd
-
-# Setup KQM and KQM Test Runner
-export GOPATH=/kqm/go
-mkdir -p /kqm/go/src/github.com/activesphere
-pushd /kqm/go/src/github.com/activesphere
-git clone https://github.com/activesphere/kqm
-popd
-
 function find_proc() {
 	echo $(ps aux | grep -i $1 | grep -v grep)
 }
@@ -59,6 +31,10 @@ function start_consumer() {
 	tail -n 5 "$1.log"
 	popd
 }
+
+echo "Installing dependencies."
+./install.sh > /dev/null 2>&1
+echo "Dependency installation complete."
 
 echo "Starting Zookeeper."
 service zookeeper start
